17032c731836ae02c0180e9675a553b5b718aea6
saka_mawa <sakadana2003@gmail.com>
2022-04-27 22:32:34 +0700

    Update log-2022-04-27.php

--- application/logs/log-2022-04-27.php
+++ application/logs/log-2022-04-27.php
@@ -10,6 +10,7 @@ ERROR - 2022-04-27 21:56:39 --> 404 Page Not Found: /index
 ERROR - 2022-04-27 21:56:39 --> 404 Page Not Found: /index
 ERROR - 2022-04-27 21:56:39 --> 404 Page Not Found: /index
 ERROR - 2022-04-27 21:56:39 --> 404 Page Not Found: /index
+<<<<<<< Updated upstream
 ERROR - 2022-04-27 22:11:06 --> 404 Page Not Found: ../../modules/dashboard/controllers//index
 ERROR - 2022-04-27 22:11:08 --> 404 Page Not Found: ../../modules/dashboard/controllers//index
 ERROR - 2022-04-27 22:29:19 --> Severity: Warning --> include(header): failed to open stream: No such file or directory C:\xampp\htdocs\lms_sd\lms_sd_manager\themes\main_frontend\layout_single.php 2
@@ -22,3 +23,7 @@ ERROR - 2022-04-27 22:29:20 --> Severity: Warning --> include(header): failed to
 ERROR - 2022-04-27 22:29:20 --> Severity: Warning --> include(): Failed opening 'header' for inclusion (include_path='C:\xampp\php\PEAR') C:\xampp\htdocs\lms_sd\lms_sd_manager\themes\main_frontend\layout_single.php 2
 ERROR - 2022-04-27 22:31:32 --> Severity: Warning --> include(header): failed to open stream: No such file or directory C:\xampp\htdocs\lms_sd\lms_sd_manager\themes\main_frontend\layout_single.php 19
 ERROR - 2022-04-27 22:31:32 --> Severity: Warning --> include(): Failed opening 'header' for inclusion (include_path='C:\xampp\php\PEAR') C:\xampp\htdocs\lms_sd\lms_sd_manager\themes\main_frontend\layout_single.php 19
+=======
+ERROR - 2022-04-27 22:02:32 --> 404 Page Not Found: /index
+ERROR - 2022-04-27 22:02:51 --> 404 Page Not Found: /index
+>>>>>>> Stashed changes
