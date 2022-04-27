35e26da0d66d00ed4379e252004211a1474a0607
saka_mawa <sakadana2003@gmail.com>
2022-04-27 21:58:35 +0700

    Update Terbaru

---
+++ application/logs/log-2022-04-27.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-04-27 21:56:37 --> 404 Page Not Found: /index
+ERROR - 2022-04-27 21:56:38 --> 404 Page Not Found: /index
+ERROR - 2022-04-27 21:56:38 --> 404 Page Not Found: /index
+ERROR - 2022-04-27 21:56:38 --> 404 Page Not Found: /index
+ERROR - 2022-04-27 21:56:39 --> 404 Page Not Found: /index
+ERROR - 2022-04-27 21:56:39 --> 404 Page Not Found: /index
+ERROR - 2022-04-27 21:56:39 --> 404 Page Not Found: /index
+ERROR - 2022-04-27 21:56:39 --> 404 Page Not Found: /index
+ERROR - 2022-04-27 21:56:39 --> 404 Page Not Found: /index
+ERROR - 2022-04-27 21:56:39 --> 404 Page Not Found: /index
